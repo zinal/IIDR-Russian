0593a5282c8f642051b303fab10bf6083be295a7
Maksim Zinal <mzinal@ru.ibm.com>
2022-02-16 18:18:22 +0300

    demo updater fixes

--- oracle-synthetic-updates/demo1.updater.main.sh
+++ oracle-synthetic-updates/demo1.updater.main.sh
@@ -10,13 +10,18 @@ sqlplus demo1/passw0rd@pdb1 <<EOF
 SET SERVEROUTPUT ON
 DECLARE
   v_id PLS_INTEGER;
+  v_rand VARCHAR2(100);
 BEGIN
   FOR i IN 1..100000 LOOP
+    v_rand := DBMS_RANDOM.STRING('x', 43);
     v_id := ( $XVAL1 * 100000) + CEIL(DBMS_RANDOM.VALUE(0, 100000));
-    UPDATE demo1 SET b=DBMS_RANDOM.STRING('x', 43), c=SYSTIMESTAMP WHERE a=v_id;
+    UPDATE demo1 SET b=v_rand, c=SYSTIMESTAMP WHERE a=v_id;
+    UPDATE demo2 SET b=v_rand, c=SYSTIMESTAMP WHERE a=v_id;
     v_id := ( $XVAL2 * 100000) + CEIL(DBMS_RANDOM.VALUE(0, 100000));
+    v_rand := DBMS_RANDOM.STRING('x', 33);
     DELETE FROM demo1 WHERE a=v_id;
-    INSERT INTO demo1(a,b,c) VALUES(v_id, DBMS_RANDOM.STRING('x',33), SYSTIMESTAMP);
+    INSERT INTO demo1(a,b,c) VALUES(v_id, v_rand, SYSTIMESTAMP);
+    UPDATE demo2 SET b=v_rand, c=SYSTIMESTAMP WHERE a=v_id;
     COMMIT;
   END LOOP;
 END;
