a61d2820e849a098a654b0d91e3b682ba0a7a43e
Maksim Zinal <mzinal@ru.ibm.com>
2022-02-16 17:46:16 +0300

    demo updater fixes

--- oracle-synthetic-updates/demo1.updater.main.sh
+++ oracle-synthetic-updates/demo1.updater.main.sh
@@ -6,17 +6,17 @@ set -u
 XVAL1="$1"
 XVAL2="$1"
 
-sqlplus cdcuser/passw0rd@wrk1 <<EOF
+sqlplus demo1/passw0rd@pdb1 <<EOF
 SET SERVEROUTPUT ON
 DECLARE
   v_id PLS_INTEGER;
 BEGIN
   FOR i IN 1..100000 LOOP
     v_id := ( $XVAL1 * 100000) + CEIL(DBMS_RANDOM.VALUE(0, 100000));
-    UPDATE demo1 SET b=DBMS_RANDOM.STRING('x', 43) WHERE a=v_id;
+    UPDATE demo1 SET b=DBMS_RANDOM.STRING('x', 43), c=SYSTIMESTAMP WHERE a=v_id;
     v_id := ( $XVAL2 * 100000) + CEIL(DBMS_RANDOM.VALUE(0, 100000));
     DELETE FROM demo1 WHERE a=v_id;
-    INSERT INTO demo1 VALUES(v_id, DBMS_RANDOM.STRING('x',33));
+    INSERT INTO demo1(a,b,c) VALUES(v_id, DBMS_RANDOM.STRING('x',33), SYSTIMESTAMP);
     COMMIT;
   END LOOP;
 END;
--- oracle-synthetic-updates/demo1.updater.run.sh
+++ oracle-synthetic-updates/demo1.updater.run.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13; do 
+for i in `seq 1 3`; do
   ( ./demo1.updater.main.sh $i >/dev/null 2>&1 &);
 done
